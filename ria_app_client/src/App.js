5400a20bed19f3ae760381beacf1908043bb7127
Emil Woźniak <emil.wozniak.591986@gmail.com>
2019-02-08 21:25:57 +0100

    Material ui

--- ria_app_client/src/App.js
+++ ria_app_client/src/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from "react";
+import compose from "recompose/compose";
+import PropTypes from "prop-types";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import store from "./store";
-import { Row, Container } from "reactstrap";
+import { Container } from "reactstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import Dashboard from "./components/Dashboard";
-import Header from "./components/layout/Header";
 import UploadFile from "./components/Upload/UploadFile";
+import Navbar from "./components/layout/ui/Navbar";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    overflow: "hidden",
+    padding: `0 ${theme.spacing.unit * 3}px`
+  },
+  paper: {
+    maxWidth: 400,
+    margin: `${theme.spacing.unit}px auto`,
+    padding: theme.spacing.unit * 2
+  }
+});
 
 class App extends Component {
+  state = {
+    direction: "row",
+    justify: "center",
+    alignItems: "center"
+  };
+
+  handleChange = key => (event, value) => {
+    this.setState({
+      [key]: value
+    });
+  };
+
   render() {
     return (
       <Provider store={store}>
         <Router>
           <div className="App">
             <Route exact path="/dashboard" component={Dashboard} />
-            <Header />
+            <Navbar />
+            <br />
             <Container>
-              <Row>
-                <UploadFile />
-              </Row>
+              <UploadFile />
               <Dashboard />
             </Container>
           </div>
@@ -30,4 +57,8 @@ class App extends Component {
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default compose(withStyles(styles))(App);
