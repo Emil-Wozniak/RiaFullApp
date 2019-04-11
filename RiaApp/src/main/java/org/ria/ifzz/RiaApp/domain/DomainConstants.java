f949170253cc4a6f1b487f2ba960d50a34942a3f
Emil Woźniak <emil.wozniak.591986@gmail.com>
2019-04-11 19:14:57 +0200

    getMatchingStrings code clean

--- RiaApp/src/main/java/org/ria/ifzz/RiaApp/domain/DomainConstants.java
+++ RiaApp/src/main/java/org/ria/ifzz/RiaApp/domain/DomainConstants.java
@@ -3,7 +3,7 @@ package org.ria.ifzz.RiaApp.domain;
 public class DomainConstants {
     public final static String HORMONE_PATTERN_UNNECESSARY_PART = "Name: COPY_OF_H-3_";
     public final static int HORMONE_PATTERN = 4;
-    public final static String METADATA_TARGET_POINT = " \tUnk";
-    public final static String FILENAME_UNNECESSARY_PART ="C:\\mbw\\results\\";
-    public final static int FILENAME_LINE = 0;
+    public final static String DATA_TARGET_POINT = " \tUnk";
+    final static String FILENAME_UNNECESSARY_PART ="C:\\mbw\\results\\";
+    final static int FILENAME_LINE = 0;
 }
--- RiaApp/src/main/java/org/ria/ifzz/RiaApp/utils/CustomFileReader.java
+++ RiaApp/src/main/java/org/ria/ifzz/RiaApp/utils/CustomFileReader.java
@@ -26,17 +26,18 @@ public class CustomFileReader {
 
     /**
      * takes metadata from uploaded file and
-     * @param metadata uploaded file
+     *
+     * @param data uploaded file
      * @return Strings containing data for
      * @throws IOException
      */
-    public List<String> readFromStream(DataFileMetadata metadata) throws IOException {
-        List<String> streamMetadata = metadata.getContents().get();
+    public List<String> readFromStream(DataFileMetadata data) throws IOException {
+        List<String> streamMetadata = data.getContents().get();
         String hormonePattern = streamMetadata.get(HORMONE_PATTERN);
         hormonePattern = hormonePattern.replace(HORMONE_PATTERN_UNNECESSARY_PART, "");
         examinationResult.add(hormonePattern);
         for (String metadataLine : streamMetadata) {
-            if (!metadataLine.startsWith(METADATA_TARGET_POINT)) {
+            if (!metadataLine.startsWith(DATA_TARGET_POINT)) {
             } else {
                 examinationResult.add(metadataLine);
             }
@@ -48,24 +49,20 @@ public class CustomFileReader {
     /**
      * Finds the all word in all entries in the list that matches with the column number
      *
-     * @param list         The list of strings to check
-     * @param columnNumber The targeted column to use
+     * @param data             The list of strings to check
+     * @param columnNumberInData The targeted column to use
      * @return list containing the words of all matching entries
      */
-    public List<String> getMatchingStrings(List<String> list, Integer columnNumber) {
-
+    public List<String> getMatchingStrings(List<String> data, Integer columnNumberInData) {
         List<String> matches = new ArrayList<>();
-        for (String added : list) {
-            List<String> wordInLine = Arrays.asList(added.split("\\t"));
-            if (wordInLine.size() == 5) {
-                if (!wordInLine.isEmpty()) {
-                    matches.add(wordInLine.get(columnNumber));
-                }
+        for (String matchData : data) {
+            List<String> dataInLine = Arrays.asList(matchData.split("\\t"));
+            if (dataInLine.size() == 5) {
+                matches.add(dataInLine.get(columnNumberInData));
             } else {
                 matches.isEmpty();
             }
         }
-//        matches.remove(0);
         return matches;
     }
 
