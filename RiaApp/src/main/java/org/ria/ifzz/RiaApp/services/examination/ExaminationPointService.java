b86ea47dd64350a5d5fdfc491420c7e02ed68803
Emil Woźniak <emil.wozniak.591986@gmail.com>
2019-07-20 20:34:44 +0200

    ExaminationPointService methods returns ResponseEntity with NOT_FOUND status and No results available message if database doesn't contain expected objects, add ExaminationPointServiceTest with tests for all methods

--- RiaApp/src/main/java/org/ria/ifzz/RiaApp/services/examination/ExaminationPointService.java
+++ RiaApp/src/main/java/org/ria/ifzz/RiaApp/services/examination/ExaminationPointService.java
@@ -9,12 +9,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ExaminationPointService extends FileExtractorImpl<ExaminationPoint> {
 
     private final ExaminationPointRepository examinationPointRepository;
+    private Map<String,String> response = new HashMap<>();
 
     public ExaminationPointService(CountResultUtil countResultUtil, ExaminationPointRepository examinationPointRepository) {
         super(new ExaminationPoint(), countResultUtil);
@@ -34,11 +38,11 @@ public class ExaminationPointService extends FileExtractorImpl<ExaminationPoint>
 
     public ResponseEntity<?> getExaminationResults() {
         List<ExaminationPoint> examinationPoints = (List<ExaminationPoint>) examinationPointRepository.findAll();
-        return new ResponseEntity<>(examinationPoints, HttpStatus.FOUND);
+        return !examinationPoints.isEmpty() ? new ResponseEntity<>(examinationPoints, HttpStatus.FOUND) : new ResponseEntity<>(response.put("message", "No results available"), HttpStatus.NOT_FOUND);
     }
 
     public ResponseEntity<?> getExaminationResultsByFilename(String filename) {
         List<ExaminationPoint> examinationPoints = examinationPointRepository.findAllByIdentifier(filename);
-        return new ResponseEntity<>(examinationPoints, HttpStatus.FOUND);
+        return !examinationPoints.isEmpty() ?  new ResponseEntity<>(examinationPoints, HttpStatus.FOUND): new ResponseEntity<>(response.put("message", "No results available"), HttpStatus.NOT_FOUND);
     }
 }
---
+++ RiaApp/src/test/java/org/ria/ifzz/RiaApp/services/examination/ExaminationPointServiceTest.java
@@ -0,0 +1,90 @@
+package org.ria.ifzz.RiaApp.services.examination;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.ria.ifzz.RiaApp.models.results.ExaminationPoint;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+
+class ExaminationPointServiceTest {
+
+    private List<ExaminationPoint> examinationPoints;
+    private ExaminationPoint examinationPoint1;
+    private ExaminationPoint examinationPoint2;
+    private ExaminationPoint examinationPoint3;
+    private ExaminationPoint examinationPoint4;
+
+    @BeforeEach
+    void setUp() {
+        examinationPoint1 = createExaminationPoint("A16_244",1,"1",200,false,"0.5");
+        examinationPoint2 = createExaminationPoint("A16_244",2,"2",300,false,"1.5");
+        examinationPoint3 = createExaminationPoint("A16_244",3,"3",20,true,"15.5");
+        examinationPoint4 = createExaminationPoint("A16_245",18,"18",2000,true,"45.5");
+        examinationPoints = new ArrayList<>();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    private ExaminationPoint createExaminationPoint(String filename, int probeNumber, String position, int cpm, boolean flag, String ng) {
+        return new ExaminationPoint(filename, "KORTYZOL_5_MIN", probeNumber, position, cpm, flag, ng);
+    }
+
+    private void fillExaminationPoints(){
+        examinationPoints.add(examinationPoint1);
+        examinationPoints.add(examinationPoint2);
+        examinationPoints.add(examinationPoint3);
+        examinationPoints.add(examinationPoint4);
+    }
+
+    @Test
+    void create_positive() {
+        fillExaminationPoints();
+        assertFalse(examinationPoints.isEmpty());
+    }
+
+    @Test
+    void create_negative() {
+        assertTrue(examinationPoints.isEmpty());
+    }
+
+    @Test
+    void getExaminationResults_positive() {
+        fillExaminationPoints();
+        List<ExaminationPoint> getExaminationPoints = examinationPoints;
+        assertNotNull(getExaminationPoints);
+        assertFalse(getExaminationPoints.isEmpty());
+    }
+
+    @Test
+    void getExaminationResults_negative() {
+        List<ExaminationPoint> getExaminationPoints = examinationPoints;
+        assertNotNull(getExaminationPoints);
+        assertTrue(getExaminationPoints.isEmpty());
+    }
+
+    @Test
+    void getExaminationResultsByFilename_positive() {
+        fillExaminationPoints();
+        List<String> indentifiers = examinationPoints.stream()
+                .map(ExaminationPoint::getIdentifier)
+                .filter(ex->ex.equals("A16_244")).collect(Collectors.toList());
+        assertTrue(indentifiers.contains("A16_244"));
+    }
+
+    @Test
+    void getExaminationResultsByFilename_negative() {
+        List<String> indentifiers = examinationPoints.stream()
+                .map(ExaminationPoint::getIdentifier)
+                .filter(ex->ex.equals("A16_244")).collect(Collectors.toList());
+        assertFalse(indentifiers.contains("A16_244"));
+    }
+}
